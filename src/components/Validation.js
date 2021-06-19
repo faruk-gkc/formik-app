064412e68e998dfae42e80df0c4ee7f75991d846
Faruk Gökcegöz <farukgokcegoz34@gmail.com>
2021-06-19 23:20:07 +0300

    yup ile validation Schema oluşturuldu.

--- src/components/Validation.js
+++ src/components/Validation.js
@@ -1,1 +1,8 @@
 import * as yup from "yup";
+
+let validationSchema = yup.object().shape({
+  name: yup.string("Geçerli bir isim girin!").required(),
+  password: yup.string(7, "Paralonız minimum 7 karakter olmalıdır!").required(),
+});
+
+export default validationSchema;
