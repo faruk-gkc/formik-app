92c59a4f9c9913b6764dd098dd0ac673495801fc
Faruk Gökcegöz <farukgokcegoz34@gmail.com>
2021-06-19 23:59:22 +0300

    Temel Fomik ve yup yapısı oluşturuldu.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,44 @@
 import React from "react";
+import { useFormik } from "formik";
+import validationSchema from "./Validation";
 
 const Login = () => {
-  return <div></div>;
+  const { values, errors, touched, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      name: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+    validationSchema,
+  });
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name : </label>
+        <input
+          name="name"
+          id="name"
+          value={values.name}
+          onChange={handleChange}
+        />
+        {errors.name && touched.name && <div>{errors.name}</div>}
+        <label htmlFor="password">Password : </label>
+        <input
+          name="password"
+          id="password"
+          value={values.password}
+          onChange={handleChange}
+        />
+        {errors.password && touched.password && <div>{errors.password}</div>}
+
+        <button type="submit">Submit</button>
+        <kbd>{JSON.stringify(values)}</kbd>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/Validation.js
+++ src/components/Validation.js
@@ -1,8 +1,11 @@
 import * as yup from "yup";
 
 let validationSchema = yup.object().shape({
-  name: yup.string("Geçerli bir isim girin!").required(),
-  password: yup.string(7, "Paralonız minimum 7 karakter olmalıdır!").required(),
+  name: yup.string().min(3, "İsminiz mininum 2 karakter olmalıdır.").required(),
+  password: yup
+    .string()
+    .min(7, "Paralonız minimum 7 karakter olmalıdır!")
+    .required(),
 });
 
 export default validationSchema;
